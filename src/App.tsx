c120d88868a803111c225a60ad1f5182ccb4237d
Preet Patel <iam@preetpatel.com>
2018-11-18 15:15:00 +1300

    added facial recognition login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Webcam from "react-webcam";
 import Modal from 'react-responsive-modal';
 import './App.css';
 import MemeDetail from './components/MemeDetail';
@@ -11,78 +12,142 @@ interface IState {
 	memes: any[],
 	open: boolean,
 	uploadFileList: any,
+	authenticated: boolean,
+	refCamera: any,
+	predictionResult: any
 }
 
 class App extends React.Component<{}, IState> {
 	constructor(props: any) {
-        super(props)
-        this.state = {
-			currentMeme: {"id":0, "title":"Loading ","url":"","tags":"⚆ _ ⚆","uploaded":"","width":"0","height":"0"},
+		super(props)
+		this.state = {
+			currentMeme: { "id": 0, "title": "Loading ", "url": "", "tags": "⚆ _ ⚆", "uploaded": "", "width": "0", "height": "0" },
 			memes: [],
 			open: false,
-			uploadFileList: null
-		}     	
+			uploadFileList: null,
+			authenticated: false,
+			refCamera: React.createRef(),
+			predictionResult: null,
+		}
 		this.selectNewMeme = this.selectNewMeme.bind(this)
 		this.fetchMemes = this.fetchMemes.bind(this)
 		this.handleFileUpload = this.handleFileUpload.bind(this)
 		this.uploadMeme = this.uploadMeme.bind(this)
-		this.fetchMemes("")	
+		this.authenticate = this.authenticate.bind(this)
+		this.fetchMemes("")
 	}
 
 	public render() {
-		const { open } = this.state;
+		const { open, authenticated } = this.state;
 		return (
-		<div>
-			<div className="header-wrapper">
-				<div className="container header">
-					<img src={PatrickLogo} height='40'/>&nbsp; My Meme Bank - MSA 2018 &nbsp;
-					<div className="btn btn-primary btn-action btn-add" onClick={this.onOpenModal}>Add Meme</div>
-				</div>
-			</div>
-			<div className="container">
-				<div className="row">
-					<div className="col-7">
-						<MemeDetail currentMeme={this.state.currentMeme} />
-					</div>
-					<div className="col-5">
-						<MemeList memes={this.state.memes} selectNewMeme={this.selectNewMeme} searchByTag={this.fetchMemes}/>
-					</div>
-				</div>
-			</div>
-			<Modal open={open} onClose={this.onCloseModal}>
-				<form>
-					<div className="form-group">
-						<label>Meme Title</label>
-						<input type="text" className="form-control" id="meme-title-input" placeholder="Enter Title" />
-						<small className="form-text text-muted">You can edit any meme later</small>
-					</div>
-					<div className="form-group">
-						<label>Tag</label>
-						<input type="text" className="form-control" id="meme-tag-input" placeholder="Enter Tag" />
-						<small className="form-text text-muted">Tag is used for search</small>
-					</div>
-					<div className="form-group">
-						<label>Image</label>
-						<input type="file" onChange={this.handleFileUpload} className="form-control-file" id="meme-image-input" />
-					</div>
+			<div>
 
-					<button type="button" className="btn" onClick={this.uploadMeme}>Upload</button>
-				</form>
-			</Modal>
-		</div>
+				{(!authenticated) ?
+					<Modal open={!authenticated} onClose={this.authenticate} closeOnOverlayClick={false} showCloseIcon={false} center={true}>
+						<Webcam
+							audio={false}
+							screenshotFormat="image/jpeg"
+							ref={this.state.refCamera}
+						/>
+						<div className="row nav-row">
+							<div className="btn btn-primary bottom-button" onClick={this.authenticate}>Login</div>
+						</div>
+					</Modal> : ""}
+				{(authenticated) ?
+					<div>
+						<div className="header-wrapper">
+							<div className="container header">
+								<img src={PatrickLogo} height='40' />&nbsp; My Meme Bank - MSA 2018 &nbsp;
+					<div className="btn btn-primary btn-action btn-add" onClick={this.onOpenModal}>Add Meme</div>
+							</div>
+						</div>
+						<div className="container">
+							<div className="row">
+								<div className="col-7">
+									<MemeDetail currentMeme={this.state.currentMeme} />
+								</div>
+								<div className="col-5">
+									<MemeList memes={this.state.memes} selectNewMeme={this.selectNewMeme} searchByTag={this.fetchMemes} />
+								</div>
+							</div>
+						</div>
+						<Modal open={open} onClose={this.onCloseModal}>
+							<form>
+								<div className="form-group">
+									<label>Meme Title</label>
+									<input type="text" className="form-control" id="meme-title-input" placeholder="Enter Title" />
+									<small className="form-text text-muted">You can edit any meme later</small>
+								</div>
+								<div className="form-group">
+									<label>Tag</label>
+									<input type="text" className="form-control" id="meme-tag-input" placeholder="Enter Tag" />
+									<small className="form-text text-muted">Tag is used for search</small>
+								</div>
+								<div className="form-group">
+									<label>Image</label>
+									<input type="file" onChange={this.handleFileUpload} className="form-control-file" id="meme-image-input" />
+								</div>
+
+								<button type="button" className="btn" onClick={this.uploadMeme}>Upload</button>
+							</form>
+						</Modal>
+					</div> : ""}
+			</div>
+
 		);
+	}
+
+	// Authenticate
+	private authenticate() {
+		const screenshot = this.state.refCamera.current.getScreenshot();
+		this.getFaceRecognitionResult(screenshot)
+	}
+
+	// Call custom vision model
+	private getFaceRecognitionResult(image: string) {
+		const url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/fd195e02-cc3a-4475-b975-056bfff17778/image?iterationId=b56a26ef-a6da-4383-923d-fc3e98186959"
+		if (image === null) {
+			return;
+		}
+		const base64 = require('base64-js');
+		const base64content = image.split(";")[1].split(",")[1]
+		const byteArray = base64.toByteArray(base64content);
+		fetch(url, {
+			body: byteArray,
+			headers: {
+				'cache-control': 'no-cache', 'Prediction-Key': 'f2467f0b9c2a4856b664c5478c7d88c0', 'Content-Type': 'application/octet-stream'
+			},
+			method: 'POST'
+		})
+			.then((response: any) => {
+				if (!response.ok) {
+					// Error State
+					alert(response.statusText)
+				} else {
+					response.json().then((json: any) => {
+						console.log(json.predictions[0])
+						this.setState({ predictionResult: json.predictions[0] })
+						if (this.state.predictionResult.probability > 0.7) {
+							this.setState({ authenticated: true })
+						} else {
+							this.setState({ authenticated: false })
+
+						}
+					})
+				}
+			})
 	}
 
 	// Modal open
 	private onOpenModal = () => {
 		this.setState({ open: true });
-	  };
-	
+	};
+
 	// Modal close
 	private onCloseModal = () => {
 		this.setState({ open: false });
 	};
-	
+
 	// Change selected meme
 	private selectNewMeme(newMeme: any) {
 		this.setState({
@@ -98,17 +163,17 @@ class App extends React.Component<{}, IState> {
 		fetch(url, {
 			method: 'GET'
 		})
-		.then(res => res.json())
-		.then(json => {
-			let currentMeme = json[0]
-			if (currentMeme === undefined) {
-				currentMeme = {"id":0, "title":"No memes (╯°□°）╯︵ ┻━┻","url":"","tags":"try a different tag","uploaded":"","width":"0","height":"0"}
-			}
-			this.setState({
-				currentMeme,
-				memes: json
-			})
-		});
+			.then(res => res.json())
+			.then(json => {
+				let currentMeme = json[0]
+				if (currentMeme === undefined) {
+					currentMeme = { "id": 0, "title": "No memes (╯°□°）╯︵ ┻━┻", "url": "", "tags": "try a different tag", "uploaded": "", "width": "0", "height": "0" }
+				}
+				this.setState({
+					currentMeme,
+					memes: json
+				})
+			});
 	}
 
 	private handleFileUpload(fileList: any) {
@@ -121,33 +186,33 @@ class App extends React.Component<{}, IState> {
 		const titleInput = document.getElementById("meme-title-input") as HTMLInputElement
 		const tagInput = document.getElementById("meme-tag-input") as HTMLInputElement
 		const imageFile = this.state.uploadFileList[0]
-	
+
 		if (titleInput === null || tagInput === null || imageFile === null) {
 			return;
 		}
-	
+
 		const title = titleInput.value
 		const tag = tagInput.value
 		const url = "http://apiformemes.azurewebsites.net/api/meme/upload"
-	
+
 		const formData = new FormData()
 		formData.append("Title", title)
 		formData.append("Tags", tag)
 		formData.append("image", imageFile)
-	
+
 		fetch(url, {
 			body: formData,
-			headers: {'cache-control': 'no-cache'},
+			headers: { 'cache-control': 'no-cache' },
 			method: 'POST'
 		})
-		.then((response : any) => {
-			if (!response.ok) {
-				// Error State
-				alert(response.statusText)
-			} else {
-				location.reload()
-			}
-		})
+			.then((response: any) => {
+				if (!response.ok) {
+					// Error State
+					alert(response.statusText)
+				} else {
+					location.reload()
+				}
+			})
 	}
 }
 
--- src/components/MemeList.tsx
+++ src/components/MemeList.tsx
@@ -1,3 +1,4 @@
+import MediaStreamRecorder from 'msr';
 import * as React from "react";
 
 interface IProps {
@@ -19,6 +20,7 @@ export default class MemeList extends React.Component<IProps, {}> {
                     <div className="input-group">
                         <input type="text" id="search-tag-textbox" className="form-control" placeholder="Search By Tags" />
                         <div className="input-group-append">
+                        <div className="btn" onClick={this.searchTagByVoice}><i className="fa fa-microphone" /></div>
                             <div className="btn btn-outline-secondary search-button" onClick = {this.searchByTag}>Search</div>
                         </div>
                     </div>  
@@ -69,6 +71,66 @@ export default class MemeList extends React.Component<IProps, {}> {
         }
         const tag = textBox.value 
         this.props.searchByTag(tag)  
+    }
+
+    private searchTagByVoice() {
+        const mediaConstraints = {
+            audio: true
+        }
+        const onMediaSuccess = (stream: any) => {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob: any) => {
+                 this.postAudio(blob);
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+    
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
+    
+        function onMediaError(e: any) {
+            console.error('media error', e);
+        }
+
+        let accessToken: any;
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0', {
+            headers: {
+                'Content-Length': '0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Ocp-Apim-Subscription-Key': 'cc9af2b91f544757b41bbae3f4cca867'
+            },
+            method: 'POST'
+        }).then((response) => {
+            // console.log(response.text())
+            return response.text()
+        }).then((response) => {
+            console.log(response)
+            accessToken = response
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+        console.log(accessToken)
+    }
+
+    private postAudio(blob:any) {
+            // posting audio
+    fetch('[YOUR API END POINT]', {
+        body: blob, // this is a .wav audio file    
+        headers: {
+            'Accept': 'application/json',
+           // 'Authorization': 'Bearer' + accessToken,
+            'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+            'Ocp-Apim-Subscription-Key': '[YOUR SUBSCRIPTION KEY]'
+        },    
+        method: 'POST'
+    }).then((res) => {
+        return res.json()
+    }).then((res: any) => {
+        console.log(res)
+    }).catch((error) => {
+        console.log("Error", error)
+    });
     }
 
 }
